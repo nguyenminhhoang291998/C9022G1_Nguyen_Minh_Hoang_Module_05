4345952e8c35308985442a3f8de62774f951ba25
nguyenminhhoang291998 <nguyenminhhoang291998@gmail.com>
2023-03-08 13:46:42 +0700

    ss4

--- case-study-furama/src/app/app.module.ts
+++ case-study-furama/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FacilityComponent } from './facility/facility.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ContractComponent } from './contract/contract.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    FacilityComponent,
+    CustomerComponent,
+    ContractComponent
   ],
   imports: [
     BrowserModule,
